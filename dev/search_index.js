7bff54296c628ad45577a393938e3158db041618
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-11-13 15:08:32 +0000

    build based on 7534dcf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BeurlingLasso-1","page":"BeurlingLasso","title":"BeurlingLasso","text":"","category":"section"},{"location":"#","page":"BeurlingLasso","title":"BeurlingLasso","text":"Coucou.","category":"page"}]
+}
